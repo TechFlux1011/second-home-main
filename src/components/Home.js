07ca336831ed255471e047fd25659fc9c4f04fee
Your Name <you@example.com>
2024-06-09 22:07:06 -0500

    fixes

--- src/components/Home.js
+++ src/components/Home.js
@@ -140,6 +140,7 @@ const Home = () => {
       /> {/* Include Navbar */}
       <div className="header-title-container">
         <h1 className="header-title">Welcome to Second Home</h1>
+        <h2 className='header-subtitle'>The dating app for clothes ❤️</h2>
       </div>
       <div className="tab-bar">
         <button className="tab">Featured Products</button>
